860f45657329df30c085526e3439b0df989ef1f7
probegi <chhr22012@g.nihon-u.ac.jp>
2023-11-01 21:03:56 +0900

    submit追加

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,12 @@ function randomValueFromArray(array) {
 
 export default function App() {
   const [showStory, setShowStory] = useState(false);
+  const [name, setName] = useState("");
+  const [temp, setTemp] = useState("");
+  const [weight, setWeight] = useState("");
   const [xItem, setXItem] = useState();
   const [yItem, setYItem] = useState();
   const [zItem, setZItem] = useState();
-  const [name, setName] = useState(""); //値保持
-  const [showName, setShowName] = useState(""); //表示
-  const [weight, setWeight] = useState("300 pounds");
-  const [temp, setTemp] = useState("94 fahrenheit");
-  const [ukus, setUkus] = useState("us");
 
   const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
   const insertY = ["the soup kitchen", "Disneyland", "the White House"];
@@ -24,61 +22,61 @@ export default function App() {
     "turned into a slug and crawled away",
   ];
 
-  function handleClick() {
-    const setStory = true;
-    setShowStory(setStory);
+  function handleSubmit(event) {
+    event.preventDefault();
+    setShowStory(true);
     setXItem(randomValueFromArray(insertX));
     setYItem(randomValueFromArray(insertY));
     setZItem(randomValueFromArray(insertZ));
-    if (name == "") {
-      setShowName("Bob");
+
+    if (event.target.elements.customname.value == "") {
+      setName("Bob");
     } else {
-      setShowName(name);
+      setName(event.target.elements.customname.value);
     }
-    if (ukus == "us") {
-      setWeight("300 pounds");
+
+    if (event.target.elements.country.value == "us") {
       setTemp("94 fahrenheit");
+      setWeight("300 pounds");
     } else {
-      setWeight("21 stone");
       setTemp("34 centigrade");
+      setWeight("21 stone");
     }
   }
 
-  const handleOptionChange = (event) => {
-    setUkus(event.target.value);
-  };
-
   return (
     <>
-      <div>
-        <label htmlFor="customname">Enter custom name:</label>
-        <input value={name} onChange={(event) => setName(event.target.value)} />
+      <div class="form">
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="customname">Enter custom name:</label>
+            <input
+              type="text"
+              name="customname"
+              class="customname"
+              placeholder=""
+            />
+          </div>
+
+          <div>
+            <label htmlFor="us">US</label>
+            <input type="radio" value="us" name="country" defaultChecked />
+            <label htmlFor="uk">UK</label>
+            <input type="radio" value="uk" name="country" />
+          </div>
+
+          <div>
+            <button type="submit">Generate random story</button>
+          </div>
+        </form>
       </div>
-      <div>
-        <label htmlFor="us">US</label>
-        <input
-          type="radio"
-          value="us"
-          checked={ukus === "us"}
-          onChange={handleOptionChange}
-        />
-        <label htmlFor="uk">UK</label>
-        <input
-          type="radio"
-          value="uk"
-          checked={ukus === "uk"}
-          onChange={handleOptionChange}
-        />
-      </div>
-      <div>
-        <button onClick={handleClick}>Generate random story</button>
-      </div>
+
       {showStory && (
         <p>
           It was {temp} outside, so {xItem} went for a walk. When they got to{" "}
-          {yItem}, they stared in horror for a few moments, then {zItem}.
-          {showName} saw the whole thing, but was not surprised —{xItem} weighs{" "}
-          {weight}, and it was a hot day.
+          {yItem}, they stared in horror for a few moments, then {zItem}. {name}{" "}
+          saw the whole thing, but was not surprised — {xItem} weighs {weight},
+          and it was a hot day.
         </p>
       )}
     </>
